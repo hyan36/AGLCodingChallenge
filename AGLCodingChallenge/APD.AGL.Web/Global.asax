56c1993e4785434e18f9170eb54a4bc7ca96c0d7
Haihao Yan <hyan@apdgroup.com>
2016-09-29 14:16:17 +1000

    add web project

---
+++ AGLCodingChallenge/APD.AGL.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="APD.AGL.Web.MvcApplication" Language="C#" %>
---
+++ AGLCodingChallenge/APD.AGL.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(APD.AGL.Web.Startup))]
+namespace APD.AGL.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
